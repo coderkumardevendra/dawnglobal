a37b826f5a9b9021af3ed2969de475e2303bcc6f
dsudesh1991 <dsudesh1991@gmail.com>
2022-07-16 04:51:20 +0530

    check commit

--- components/Team.js
+++ components/Team.js
@@ -9,7 +9,7 @@ export default function Team() {
     return (
         <div className='teams wrapper px-70'>
             <h4>Meet the <span>Team</span></h4>
-            <div className='card-wrapper'>{isSuccess && team.map(t => <TeamCard name={t.name} position={t.position} profile_image={t.profile_image.id} intro={t.intro} key={t.id} />)}</div>
+            
         </div>
     )
 }
